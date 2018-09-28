f6e17fb26d2e79ca5636185809e4b2fa419f2b80
lakshyajit165 <lakshyajit165@gmail.com>
2018-09-28 15:52:30 +0530

    Contact page mail functionality

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 //HandleBars Middleware
 app.engine('handlebars', 
@@ -72,4 +80,63 @@ app.get('/gallery', (req,res) => {
 app.get('/contact' ,(req,res) => {
 	res.render('contact');
 });
+
+app.post('/send', (req,res) => {
+	
+  
+
+            const output = `
+
+        		<p>You have a new contact request!</p>
+        		<h3>Contact Details</h3>
+        		<ul>
+        			<li>Email: ${req.body.email}</li>
+        			<li>Name : ${req.body.name}</li>
+        			<li>Phone: ${req.body.phone}</li>
+        		</ul>
+        		<h3>Message</h3>
+        		<p>${req.body.message}</p>
+        	`;
+
+        	// create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+                host: 'smtp.stackmail.com', //'smtp.ethereal.email',
+                port: 587,//587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: 'contact@iamlakshyajit.in', // generated ethereal user
+                    pass: 'gagool123'//account.pass ''// generated ethereal password
+                },
+                tls:{
+                	rejectUnauthorized: false
+                }
+            });
+
+            // setup email data with unicode symbols
+            let mailOptions = {
+                from: '"<careermantra.net.in>', // sender address
+                to: 'info@careermantra.net.in', // list of receivers
+                subject: 'New Message', // Subject line
+                text: 'Hello world?', // plain text body
+                html: output // html body
+            };
+
+            // send mail with defined transport object
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                	res.render('contact',{msg:'Message not sent...Please try again!',val:'alert alert-danger'});
+                    return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+                // Preview only available when sending through an Ethereal account
+                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+                res.render('contact',{msg:'Message sent..Will get back to you soon!',val:'alert alert-success'});
+
+                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+                // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+            });
+           
+});
+
 app.listen(process.env.PORT || 3000, () => console.log('Server Started...'));
